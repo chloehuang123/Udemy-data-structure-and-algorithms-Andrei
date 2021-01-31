bebf2f55ff3ea068302dc8e4d7f11014cf643618
cubasve <eva.cubasvasquez@gmail.com>
2021-01-30 23:51:05 -0500

    Add set and get methods to hash table code

--- DataStructures/HashTable/hashTable.js
+++ DataStructures/HashTable/hashTable.js
@@ -18,16 +18,51 @@ class HashTable {
         this.data = new Array(size);
     }
 
-    _hash(key) {
+    //_ = private property that you should not be accessing outside of the class
+    _hash(key) { //O(1)
         let hash = 0;
-        for (let i = 0; i < key.length; i++) {
+        for (let i = 0; i < key.length; i++) {//grab the length of 'grapes' (key)
             hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+            //charCodeAt(): returns an integer between 0 & 65535 
+            //this.data.length: ensures the size stays between 0-50 (memory space for hash table)
+            console.log(hash); // 0 14 8 44 48 23
         }
         return hash;
+    }
+
+    set(key, value) { //O(1)
+        //address = where we want to store info
+        let address = this._hash(key); //store key & value in address space created by _hash function
+        if (!this.data[address]) {
+            this.data[address] = [];
+            this.data[address].push([key, value]);
+            console.log(this.data); //[<23 empty items>, [['grapes, 1000]], <26 empty items> ]
+        }
+        //ELSE? data already exists there (collision) - add onto that array
+        this.data[address].push([key, value]);
+        return this.data;
+    }
+
+    get(key) { //O(1)
+        let address = this._hash(key);
+        const currentBucket = this.data[address];
+        if (currentBucket) { //if there is something there (length > 0)
+            for (let i = 0; i < currentBucket.length; i++) { //loop bc we may have >1 item
+                if (currentBucket[i][0] === key) {
+                    return currentBucket[i][1];
+                }
+            }
+
+        }
+        return undefined;
     }
 }
 
 const myHashTable = new HashTable(50); //give hash table memory size of 50
-myHashTable._hash('grapes'); //23 - memory space is from 0-50
-myHashTable.set('grapes', 1000);
+//myHashTable._hash('grapes'); //23 - memory space is from 0-50
+
+myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 54);
+/* Memory space of 2 example */
+//[<1 empty item>, [ ['grapes, 1000], ['apples', 54] ] ]
 myHashTable.get('grapes');
